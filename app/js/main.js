cc4219389f39edaff3fb6efc7e66cb40accfe5e3
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-11-22 20:05:48 +0300

    added styles and scripts for menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.header__list-item').on('click', function(){
+    $('.header__list-item').removeClass('header__list-item_active');
+    $(this).addClass('header__list-item_active');
+    })
 })
