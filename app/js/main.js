dc8d3c0554fef4c5080524b61807bf52fbbfcc6f
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-11-22 20:18:48 +0300

    added scripts for themes button

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,9 @@ $(function(){
     $('.header__list-item').removeClass('header__list-item_active');
     $(this).addClass('header__list-item_active');
     })
+
+    $('.themes').on('click', function(){
+        $('.themes').toggleClass('themes_active');
+        $('.themes__switch').toggleClass('themes__switch_active');
+    })
 })
